1b27e79a82fde8da79fa44f0789a24a789397cba
VetaPol <wio2303letta@gmail.com>
2023-01-03 21:16:39 +0100

    Initial commit

---
+++ src/Lesson1.java
@@ -0,0 +1,16 @@
+public class Lesson1 {
+    public static void main(String[] args) {
+
+        int a = 3;
+        int b = 3;
+        int c = 333;
+
+        int result = a+b;
+
+        int result2 = result * c;
+
+        System.out.println(result2);
+    }
+
+
+}
---
+++ src/Lesson2.java
@@ -0,0 +1,9 @@
+public class Lesson2 {
+    public static void main(String[] args) {
+
+        String t3 = "You are beauty";
+
+
+        System.out.println(t3);
+    }
+}
